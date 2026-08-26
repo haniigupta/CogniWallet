0dc9a870ca85ac0aa5147ce5d31152999547fd20
guptahani75@gmail.com <guptahani75@gmail.com>
2026-08-26 17:55:53 +0530

    setup neon postgres connection pool

---
+++ backend/utils/db.js
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+import pkg from 'pg';
+
+dotenv.config();
+
+const { Pool, types} = pkg;
+
+// return date col (OID 1082) as plain 'YYYY-MM-DD' string as plain json
+
+types.setTypeParser(1082, (val) => val);
+
+const pool = new Pool({
+    connectionString : process.env.DATABSE_URL,
+    ssl : { rejectUnauthorized: false},
+})
+
+pool.on('connect' , () => {
+    console.log('Connect to neon postgres!');
+})
+
+pool.on('error', (err) => {
+    console.error('unexpected postgres error:', err);
+    process.exit(-1);
+})
+
+export default pool; 
