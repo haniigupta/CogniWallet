615bc0904d78e0847e54beaf068fe7f170afcb51
guptahani75@gmail.com <guptahani75@gmail.com>
2026-08-28 01:17:36 +0530

    import category route to server

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv'
 
 import authRoutes from './routes/authRoutes.js'
+import categoryRoutes from './routes/categoryRoutes.js'
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ app.get('/', (req,res) => {
 })
 
 app.use('/api/auth', authRoutes);
+app.use('/api/categories', categoryRoutes)
 
 app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}`)
