e9067f2a87e184a05dbba81d310dace424ea19d7
guptahani75@gmail.com <guptahani75@gmail.com>
2026-08-30 23:11:01 +0530

    add delete transaction controller

--- backend/controllers/transactionController.js
+++ backend/controllers/transactionController.js
@@ -149,3 +149,34 @@ export const updateTransaction = async (req, res) => {
     }
     
 }
+
+export const deleteTransaction = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+
+        const result = await pool.query(
+            `DELETE FROM transactions
+            WHERE id = $1 AND user_id = $2
+            RETURNING *`,
+            [id, req.userId]
+        )
+
+        if(result.rows.length === 0){
+            return res.status(404).json({
+                message : 'Transaction not found!'
+            })
+        }
+
+        res.json({
+            message : 'Transaction deleted successfully!',
+            transaction : result.rows[0]
+        })
+
+    } catch (error){
+        console.error('deleteTransaction error', error)
+        res.status(500).json({ message : 'Server error'})
+    }
+}
+
+
