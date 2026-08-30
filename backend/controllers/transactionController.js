c335cea12f447e22508413e91832474dd368d442
guptahani75@gmail.com <guptahani75@gmail.com>
2026-08-30 23:07:46 +0530

    add update transaction controller

--- backend/controllers/transactionController.js
+++ backend/controllers/transactionController.js
@@ -117,3 +117,35 @@ export const getTransactionById = async (req, res) => {
         res.status(500).json({ message : 'Server error'})
     }
 }
+
+export const updateTransaction = async (req, res) => {
+    const { id } = req.params;
+    const { categoryId, amount, type, description, notes, transactionDate } = req.body
+
+    try {
+
+        const result = await pool.query(
+            `UPDATE transactions
+            SET category_id = COALESCE($1, category_id),
+                amount = COALESCE($2, amount),
+                type = COALESCE($3, type),
+                description = COALESCE($4, description),
+                notes = COALESCE($5, notes),
+                transaction_date = COALESCE($6, transaction_date)
+            WHERE id = $7 AND user_id = $8
+            RETURNING *`,
+            [categoryId, amount, type, description, notes, transactionDate, id, req.userId]
+        )
+        if(result.rows.length === 0){
+            return res.status(404).json({
+                message : 'Transaction not found!'
+            })
+        }
+        res.json(result.rows[0])
+
+    }catch (error){
+        console.error('updateTransaction error', error)
+        res.status(500).json({ message : 'Server error'})
+    }
+    
+}
